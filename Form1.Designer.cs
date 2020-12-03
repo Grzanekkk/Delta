d7ead09663a5cf22495f3ba07b41ae3ddc524ed5
Jan Puto <tofukles@gmail.com>
2020-12-03 10:36:37 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace Delta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.aInput = new System.Windows.Forms.TextBox();
+            this.bInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iloczynowaTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kanonicznaTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ogolnaTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PostacieLabel = new System.Windows.Forms.Label();
+            this.ObliczButton = new System.Windows.Forms.Button();
+            this.wierzcholekTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deltaTxt = new System.Windows.Forms.TextBox();
+            this.cInput = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.miejsceZeroweTxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(42, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            // 
+            // aInput
+            // 
+            this.aInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.aInput.Location = new System.Drawing.Point(74, 33);
+            this.aInput.Name = "aInput";
+            this.aInput.Size = new System.Drawing.Size(100, 30);
+            this.aInput.TabIndex = 1;
+            // 
+            // bInput
+            // 
+            this.bInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bInput.Location = new System.Drawing.Point(74, 69);
+            this.bInput.Name = "bInput";
+            this.bInput.Size = new System.Drawing.Size(100, 30);
+            this.bInput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(42, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B";
+            // 
+            // iloczynowaTxt
+            // 
+            this.iloczynowaTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.iloczynowaTxt.Location = new System.Drawing.Point(145, 367);
+            this.iloczynowaTxt.Name = "iloczynowaTxt";
+            this.iloczynowaTxt.Size = new System.Drawing.Size(228, 30);
+            this.iloczynowaTxt.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(30, 370);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Iloczynowa";
+            // 
+            // kanonicznaTxt
+            // 
+            this.kanonicznaTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kanonicznaTxt.Location = new System.Drawing.Point(145, 328);
+            this.kanonicznaTxt.Name = "kanonicznaTxt";
+            this.kanonicznaTxt.Size = new System.Drawing.Size(228, 30);
+            this.kanonicznaTxt.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(23, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kanoniczna";
+            // 
+            // ogolnaTxt
+            // 
+            this.ogolnaTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ogolnaTxt.Location = new System.Drawing.Point(145, 292);
+            this.ogolnaTxt.Name = "ogolnaTxt";
+            this.ogolnaTxt.Size = new System.Drawing.Size(228, 30);
+            this.ogolnaTxt.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(63, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Ogólna";
+            // 
+            // PostacieLabel
+            // 
+            this.PostacieLabel.AutoSize = true;
+            this.PostacieLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PostacieLabel.Location = new System.Drawing.Point(102, 248);
+            this.PostacieLabel.Name = "PostacieLabel";
+            this.PostacieLabel.Size = new System.Drawing.Size(87, 25);
+            this.PostacieLabel.TabIndex = 12;
+            this.PostacieLabel.Text = "Postacie";
+            // 
+            // ObliczButton
+            // 
+            this.ObliczButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ObliczButton.Location = new System.Drawing.Point(332, 424);
+            this.ObliczButton.Name = "ObliczButton";
+            this.ObliczButton.Size = new System.Drawing.Size(94, 44);
+            this.ObliczButton.TabIndex = 13;
+            this.ObliczButton.Text = "Oblicz";
+            this.ObliczButton.UseVisualStyleBackColor = true;
+            this.ObliczButton.Click += new System.EventHandler(this.ObliczButton_Click);
+            // 
+            // wierzcholekTxt
+            // 
+            this.wierzcholekTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wierzcholekTxt.Location = new System.Drawing.Point(565, 331);
+            this.wierzcholekTxt.Name = "wierzcholekTxt";
+            this.wierzcholekTxt.Size = new System.Drawing.Size(256, 30);
+            this.wierzcholekTxt.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(439, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Wierzchołek";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Delta";
+            // 
+            // deltaTxt
+            // 
+            this.deltaTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.deltaTxt.Location = new System.Drawing.Point(75, 187);
+            this.deltaTxt.Name = "deltaTxt";
+            this.deltaTxt.Size = new System.Drawing.Size(100, 30);
+            this.deltaTxt.TabIndex = 5;
+            // 
+            // cInput
+            // 
+            this.cInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cInput.Location = new System.Drawing.Point(74, 105);
+            this.cInput.Name = "cInput";
+            this.cInput.Size = new System.Drawing.Size(100, 30);
+            this.cInput.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(40, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "C";
+            // 
+            // miejsceZeroweTxt
+            // 
+            this.miejsceZeroweTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.miejsceZeroweTxt.Location = new System.Drawing.Point(565, 370);
+            this.miejsceZeroweTxt.Name = "miejsceZeroweTxt";
+            this.miejsceZeroweTxt.Size = new System.Drawing.Size(256, 30);
+            this.miejsceZeroweTxt.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(413, 373);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(149, 25);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Miejsca Zerowe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 484);
+            this.Controls.Add(this.miejsceZeroweTxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cInput);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.wierzcholekTxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ObliczButton);
+            this.Controls.Add(this.PostacieLabel);
+            this.Controls.Add(this.iloczynowaTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.kanonicznaTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ogolnaTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.deltaTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.aInput);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Oblicznie Delty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox aInput;
+        private System.Windows.Forms.TextBox bInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox iloczynowaTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox kanonicznaTxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ogolnaTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label PostacieLabel;
+        private System.Windows.Forms.Button ObliczButton;
+        private System.Windows.Forms.TextBox wierzcholekTxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox deltaTxt;
+        private System.Windows.Forms.TextBox cInput;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox miejsceZeroweTxt;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Windows.Forms;
+using System.Windows.Media;
+using LinqToDB;
+
+namespace Delta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double delta;
+        double a, b, c;
+        double p, q;
+        double x1, x2;
+
+        string postacKanoniczna, postacOgolna, postacIloczynowa, wierzcholek, miejscaZerowe;
+
+        private void ObliczButton_Click(object sender, EventArgs e)
+        {
+            PrzypiszABC();
+            ObliczWKolejnosci();
+            WyświetlDane();
+            WyświetlWykres();
+        }
+
+        void WyświetlDane()
+        {
+            ogolnaTxt.Text = postacOgolna;
+            kanonicznaTxt.Text = postacKanoniczna;
+            iloczynowaTxt.Text = postacIloczynowa;
+            wierzcholekTxt.Text = wierzcholek;
+            miejsceZeroweTxt.Text = miejscaZerowe;
+            deltaTxt.Text = delta.ToString();
+
+        }
+
+        void WyświetlWykres()
+        {
+
+
+        }
+
+        void ObliczWKolejnosci()
+        {
+            ObliczDelte();
+
+            ObliczWierzchołek();
+            ObliczMiejscaZerowe();
+
+            ObliczPostacOgolna();
+            ObliczPostacKanoniczna();
+            ObliczPostacIloczynowa();
+        }
+
+
+        void PrzypiszABC()
+        {
+            a = Convert.ToDouble(aInput.Text);
+            b = Convert.ToDouble(bInput.Text);
+            c = Convert.ToDouble(cInput.Text);
+        }
+
+
+        #region Liczenie
+
+
+        void ObliczDelte()
+        {
+            delta = (b * b) - (4 * a * c);
+        }
+
+        void ObliczWierzchołek()
+        {
+            p = -b / 2 * a;
+            q = -delta / 4 * a;
+
+            wierzcholek = $"({p}, {q})";
+        }
+
+        void ObliczMiejscaZerowe()
+        {
+            if(delta > 0)
+            {
+                x1 = Math.Round((-b - Math.Sqrt(delta)) / 2 * a, 2);
+                x2 = Math.Round((-b + Math.Sqrt(delta)) / 2 * a, 2);
+
+                miejscaZerowe = $"x1 = {x1}, x2 = {x2}";
+            }
+            else if(delta == 0)
+            {
+                x1 = Math.Round((-b - Math.Sqrt(delta)) / 2 * a, 2);
+                miejscaZerowe = $"{x1}";
+            }
+            else if(delta < 0)
+            {
+                miejscaZerowe = "Brak";
+            }        
+        }
+
+        void ObliczPostacOgolna()
+        {
+            if (b < 0)
+            {
+                if(c < 0)
+                {
+                    postacOgolna = $"{a}x² - {Math.Abs(b)}x - {Math.Abs(c)}";
+                    return;
+                }
+
+                postacOgolna = $"{a}x² - {Math.Abs(b)}x + {c}";
+                return;
+            }
+
+            if (c < 0)
+            {
+                postacOgolna = $"{a}x² + {b}x - {Math.Abs(c)}";
+                return;
+            }
+
+            postacOgolna = $"{a}x² + {b}x + {c}";
+        }
+
+        void ObliczPostacKanoniczna()
+        {
+            if (p < 0)
+            {
+                if (q < 0)
+                {
+                    postacKanoniczna = $"{a}(x + {Math.Abs(p)})² - {Math.Abs(q)}";
+                    return;
+                }
+
+                postacKanoniczna = $"{a}(x + {Math.Abs(p)})² + {q}";
+                return;
+            }
+
+            if (q < 0)
+            {
+                postacKanoniczna = $"{a}(x - {p})² - {Math.Abs(q)}";
+                return;
+            }
+
+            postacKanoniczna = $"{a}(x - {p})² + {q}";
+        }
+
+        void ObliczPostacIloczynowa()
+        {
+            if (delta < 0)
+            {
+                postacIloczynowa = "Brak";
+                return;
+            }
+
+            if(delta == 0)
+            {
+                x2 = x1;
+            }
+
+            if (x1 < 0)
+            {
+                if (x2 < 0)
+                {
+                    postacIloczynowa = $"{a}(x + {Math.Abs(x1)})(x + {Math.Abs(x2)})";
+                    return;
+                }
+
+                postacIloczynowa = $"{a}(x + {Math.Abs(x1)})(x - {x2})";
+                return;
+            }
+
+            if (x2 < 0)
+            {
+                postacIloczynowa = $"{a}(x - {x1})(x + {Math.Abs(x2)})";
+                return;
+            }
+
+            postacIloczynowa = $"{a}(x - {x1})(x - {x2})";
+        }
+
+
+        #endregion Liczenie
+    }
+}
